4347421d86821053c43bbad4f209ea862efd8d77
Mikhail Shlykov <shlykov_misha_2008@mail.ru>
2022-08-20 13:41:41 +0300

    feat: works

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use bevy::prelude::{*, window::WindowMode};
+
 fn main() {
-    println!("Hello, world!");
+    std::env::set_var("WGPU_BACKEND", "Vulkan");
+
+    App::new()
+         .insert_resourse(WindowDescriptor {
+            title: "Крутой заголовок окна".into(),
+            // decorations: false,
+            mode: WindowMode::BorderlessFullscreen,
+            ..Default::default()
+         })
+         .add_plugins(DefaultPlugins)
+         .run();
 }
