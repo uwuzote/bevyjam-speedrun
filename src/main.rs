09e6ac7efa23a1c4e1a5f81c18fea696274ee837
Mikhail Shlykov <shlykov_misha_2008@mail.ru>
2022-08-20 13:42:58 +0300

    fix: now works

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-use bevy::prelude::{*, window::WindowMode};
+use bevy::{prelude::*, window::WindowMode};
 
 fn main() {
     std::env::set_var("WGPU_BACKEND", "Vulkan");
 
     App::new()
-         .insert_resourse(WindowDescriptor {
+         .insert_resource(WindowDescriptor {
             title: "Крутой заголовок окна".into(),
             // decorations: false,
             mode: WindowMode::BorderlessFullscreen,
