b0d38764483d44f566efb44d66bb21cb56ae01e6
Mikhail Shlykov <shlykov_misha_2008@mail.ru>
2022-08-21 15:50:20 +0300

    feat: movement and menu (phantom)

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 mod textures;
 mod spawn;
 
-use bevy::{prelude::*, window::WindowMode, render::texture::ImageSettings};
+use bevy::{prelude::*, app::AppExit, window::WindowMode, render::texture::ImageSettings};
 use textures::*;
 use spawn::*;
 
@@ -21,31 +21,87 @@ fn main() {
            mode: WindowMode::BorderlessFullscreen,
            ..default()
         })
+        .add_state(GameState::Game)
         .add_plugins(DefaultPlugins)
         .init_resource::<TileSheet>()
         .init_resource::<ItemSheet>()
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_koci4)
-        // .add_system(animate_demons)
+        .add_system(toggle_menu)
+        .add_system_set(
+            SystemSet::on_update(GameState::Game)
+            .with_system(move_active_demon)
+        )
+        .add_system_set(
+            SystemSet::on_update(GameState::Items)
+            .with_system(quit_game)
+        )
         .run();
 }
+
+#[derive(Debug, Clone, Eq, PartialEq, Hash)]
+enum GameState {
+    // MainMenu,
+    Game,
+    Items,
+}
+
+#[derive(Component)]
+struct ActiveDemon;
 
 fn spawn_camera(mut cmd: Commands) {
     cmd.spawn_bundle(Camera2dBundle::default());
 }
 
+fn quit_game(mut exit: EventWriter<AppExit>, keys: Res<Input<KeyCode>>) {
+    if keys.pressed(KeyCode::Escape) {
+        exit.send(AppExit);
+    }
+}
+
+fn toggle_menu(
+    keys: Res<Input<KeyCode>>,
+    mut state: ResMut<State<GameState>>
+) {
+    if keys.just_pressed(KeyCode::E) {
+        let newstate = match state.current() {
+            GameState::Game => GameState::Items,
+            GameState::Items => GameState::Game
+        };
+
+        state.set(newstate).unwrap();
+    };
+}
+
 fn spawn_koci4(mut cmd: Commands, tiles: Res<TileSheet>) {
-    cmd.spawn_bundle(DemonBundle::new([0.0, 0.0], 0, &tiles));
+    cmd.spawn_bundle(DemonBundle::new([0.0, 0.0], 0, &tiles)).insert(ActiveDemon);
     cmd.spawn_bundle(TileBundle::new([1.0, 1.0], 1, &tiles));
     cmd.spawn_bundle(TileBundle::new([0.0, 1.0], 2, &tiles));
     cmd.spawn_bundle(TileBundle::new([1.0, 0.0], 2, &tiles));
 }
 
-// fn animate_demons(mut query: Query<&mut Transform, With<Demon>>, time: Res<Time>) {
-//     for mut demon in &mut query {
-//         demon.translation += anim;
-//     };
-// }
+fn move_active_demon(
+    mut query: Query<&mut Transform, With<ActiveDemon>>,
+    keys: Res<Input<KeyCode>>
+) {
+    let mut active = query.single_mut();
+
+    if keys.just_pressed(KeyCode::D) {
+        active.translation += Vec3::new(TILE_OFF, 0.0, 0.0);
+    };
+
+    if keys.just_pressed(KeyCode::A) {
+        active.translation += Vec3::new(-TILE_OFF, 0.0, 0.0);
+    };
+
+    if keys.just_pressed(KeyCode::W) {
+        active.translation += Vec3::new(0.0, TILE_OFF, 0.0);
+    };
+
+    if keys.just_pressed(KeyCode::S) {
+        active.translation += Vec3::new(0.0, -TILE_OFF, 0.0);
+    };
+}
 
 // fn animate_sprite(
 //     time: Res<Time>,
